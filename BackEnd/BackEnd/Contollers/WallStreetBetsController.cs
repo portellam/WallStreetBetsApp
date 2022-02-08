9594450cfaaa3a636fd23b645c0db422223a2ae2
coloritoj <coloritoj@gmail.com>
2022-02-08 13:22:31 -0500

    sending over to alex

--- BackEnd/BackEnd/Contollers/WallStreetBetsController.cs
+++ BackEnd/BackEnd/Contollers/WallStreetBetsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BackEnd.Controllers;
+using BackEnd.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,20 +31,33 @@ namespace BackEnd.Contollers
         }
 
         // function creates a User, adds to list
-        [HttpGet]
-        public void PostUser(User username, User firstname)
-        { 
+        [HttpPost]
+        public void PostUser(string username, string first_name)
+        {
             // we are taking in values, which is a username and first_name, and adding to list
             // do I instantiate a list of users here, or call _context.Users ?
 
+            User myUser = new User();
+            myUser.username = username;
+            myUser.first_name = first_name;
+
+            _context.Users.Add(myUser);
+            _context.SaveChanges();
+
+            // EXAMPLE: https://localhost:7262/api/WallStreetBets?username=jeffcogs&first_name=jeff
         }
 
-        // function updates a User (given no name conflicts with another User username)
-        [HttpGet]
-        public void PutUser()
-        { 
-        
+        /*
+        [HttpPut]
+        public void EditUser(string currentUsername, string newUsername, string newFirstName)
+        {
+            
         }
+        */
+
+
+
+        /*
 
         // function deletes a User (also should delete all Favs/Notes of given User, too)
         [HttpGet]
@@ -59,23 +73,35 @@ namespace BackEnd.Contollers
             }
             return;
         }
+        */
+
 
         // TODO:    Favorites table
 
         // function reads Favorite Ticker
         // GET api/<WallStreetBetsController>/5
         [HttpGet("{id}")]
-        public IEnumerable<Favorite> GetFavs()
+        public static List<Favorite> GetFavs(string username)
         {
-            return "JoshC";
+            List<Favorite> result = null;
+            using (WallStreetBetsContext ctx = new WallStreetBetsContext())
+            {
+                var query = 
+            }
         }
+
+
+
+
+        /*
+
 
         // function creates Favorite Ticker, assigns to a User
         // POST api/<WallStreetBetsController>
         [HttpPost]
-        public void PostFav(string username, string ticker, _context)
+        public void PostFav(string username, string ticker)
         {
-            if(
+            
 
         }
 
@@ -97,7 +123,7 @@ namespace BackEnd.Contollers
 
 
        
-
+        */
 
     }
 }
