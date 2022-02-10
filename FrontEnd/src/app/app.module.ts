868262aa33e051912fc1bf54ca50f54e145c12e6
coloritoj <coloritoj@gmail.com>
2022-02-10 01:27:46 -0500

    we might have to only allow one note per stock

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AdvancedJoinComponent } from './advanced-join/advanced-join.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    FavoriteComponent
+    FavoriteComponent,
+    AdvancedJoinComponent
   ],
   imports: [
     BrowserModule,
--- FrontEnd/src/app/favorite/favorite.component.ts
+++ FrontEnd/src/app/favorite/favorite.component.ts
@@ -24,6 +24,8 @@ export class FavoriteComponent implements OnInit {
       }
     );
   }
+
+  
   
   
 }
