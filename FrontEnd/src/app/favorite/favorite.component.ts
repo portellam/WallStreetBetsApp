7cb8da768d5575987bcf71c82b58d9869e358047
coloritoj <coloritoj@gmail.com>
2022-02-10 02:11:07 -0500

    editStockNote function is working in test stage but is not complete

--- FrontEnd/src/app/favorite/favorite.component.ts
+++ FrontEnd/src/app/favorite/favorite.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { JoinResults } from '../join-results';
 import { JoinResultsService } from '../join-results.service';
+import { EditNote } from '../edit-note';
+import { EditNoteService } from '../edit-note.service';
 
 @Component({
   selector: 'app-favorite',
@@ -11,9 +13,13 @@ export class FavoriteComponent implements OnInit {
 
   allJoinResultsForUser: JoinResults[] = [];
 
+  _EditNote: EditNote = {
+    noteID: 25,
+    updatedNoteDescription: 'testing this'
+  }
 
-
-  constructor(private JoinResultsService: JoinResultsService) { }
+  constructor(private JoinResultsService: JoinResultsService,
+    private EditNoteService: EditNoteService) { }
 
   ngOnInit(): void {
     this.getUserJoinResults();
@@ -27,8 +33,15 @@ export class FavoriteComponent implements OnInit {
     );
   }
   
-  addNewNote(){
-    
+  editStockNote() {
+    this.EditNoteService.editNote(this._EditNote, 
+      (result: any) => {
+        alert('Successfully updated the test note');
+        this.getUserJoinResults();
+      }
+    );
   }
-  
+  // Example URL: https://localhost:7262/api/WallStreetBets/notes?noteID=25&updatedNoteDescription=this%20looks%20like%20a%20great%20stock
+
+
 }
