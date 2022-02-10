79ee6b0ba398588b1f05d835c066f381ff1cafa6
Alex Portell <amportell.13@gmail.com>
2022-02-10 14:48:44 +0000

    Update WallStreetBetsController.cs
    Added comments

--- BackEnd/BackEnd/Contollers/WallStreetBetsController.cs
+++ BackEnd/BackEnd/Contollers/WallStreetBetsController.cs
@@ -12,38 +12,86 @@ namespace BackEnd.Contollers
 	public class WallStreetBetsController : ControllerBase
 	{
         // Database
-		private readonly WallStreetBetsContext _context;
+	private readonly WallStreetBetsContext _context;
 
         // METHODS //
-		public WallStreetBetsController(WallStreetBetsContext context)
-		{
-			_context = context;
-		}
-        // ==================================================================================================== //
-
-        // User table CRUD
-
+	public WallStreetBetsController(WallStreetBetsContext context)
+	{
+		_context = context;
+	}
+		
+	// CRUD FUNCTIONS
+	
+        // User table
+	
+	// function reads the list of Users.
         [HttpGet]
-		public IEnumerable<User> GetUsers()
-		{
-			return _context.Users;
-		}
+	public IEnumerable<User> GetUsers()
+	{
+		return _context.Users;
+	}
+	
+	// function edits User, checks for conflicting match.
+	[HttpPost]
+	public void PostUser(string username, string first_name)
+	{
+		User myUser = new User();
+		myUser.username = username;
+		myUser.first_name = first_name;
+		_context.Users.Add(myUser);
+		_context.SaveChanges();
+	        // URL: https://localhost:7262/api/WallStreetBets?username=jeffcogs&first_name=jeff
+	}
+	/*
 
-		[HttpPost]
-		public void PostUser(string username, string first_name)
-		{
-			User myUser = new User();
-			myUser.username = username;
-			myUser.first_name = first_name;
-			_context.Users.Add(myUser);
-			_context.SaveChanges();
-            // URL: https://localhost:7262/api/WallStreetBets?username=jeffcogs&first_name=jeff
-		}
+	// function edits User, checks for conflicting match.
+        [HttpPut]
+        public void EditUser(int id, string username, string first_name)
+        {
+            // FrontEnd:
+            // verify input, if string is null or same, assume no change and pass old string
+            // else, pass new string
+            List<User> userList = _context.Users.ToList();
+            for (int i = 0; i < userList.Count; i++)
+            {
+                if (id != i && username == userList[i].username)
+                {
+                    return;     // If user is not current user and match exists, exit function now.
+                }
+            }
+            User myUser = userList.ElementAt(id);
+            myUser.username = username;
+            myUser.firstName = first_name;
+            _context.Users.Update(myUser);
+            _context.SaveChanges();
+        }
+	*/
+
+        // function deletes a User.
+	/*
+        [HttpDelete]
+        public void DeleteUser(int id)
+        {
+            List<User> userList = _context.Users.ToList();
+            for (int i = 0; i < userList.Count; i++)
+            {
+                if (id == userList[i].id)
+                {
+                    // TODO: recursive call DeleteNote(), and DeleteFavorite() here?
+                    _context.Users.Remove(userList.ElementAt(id));
+                    _context.SaveChanges();
+                    return;     // If match exists, exit function now.
+                }
+            }
+        }
+	*/
+
         // ==================================================================================================== //
 
         // Favorite table CRUD
-
         // NOTE: Put/Edit is not necessary for Favorite
+	
+	// function reads list of Favorites.
         [Route("favorites")]
         [HttpGet]
         public IEnumerable<Favorite> GetFavorites()
@@ -51,6 +99,7 @@ namespace BackEnd.Contollers
             return _context.Favorites;
         }
 
+	// function creates Favorite, assigns to a User.
         [Route("favorites")]
         [HttpPost]
         public void AddFav(string username, string ticker)
@@ -69,7 +118,8 @@ namespace BackEnd.Contollers
             _context.Favorites.Add(newFav);
             _context.SaveChanges();
         }
-
+	
+	// function deletes Favorite.
         [Route("favorites")]
         [HttpDelete]
         public void DeleteFav(string username, string ticker)
@@ -89,6 +139,7 @@ namespace BackEnd.Contollers
 
         // Note table CRUD
 
+	// function reads list of Notes.
         [Route("notes")]
         [HttpGet]
         public IEnumerable<Note> GetNotes()
@@ -96,6 +147,7 @@ namespace BackEnd.Contollers
             return _context.Notes;
         }
 
+	// function creates Note, assigns to Favorite Ticker.
         [Route("notes")]
         [HttpPost]
         public void AddNote(int favID, string noteDescription)
@@ -116,6 +168,7 @@ namespace BackEnd.Contollers
             }
         }
 
+	// function edits Note.
         [Route("notes")]
         [HttpPut]
         public void EditNote(int noteID, string updatedNoteDescription)
@@ -136,6 +189,7 @@ namespace BackEnd.Contollers
             }
         }
 
+	// function deletes Note.
         [Route("notes")]
         [HttpDelete]
         public void DeleteNote(int noteID)
@@ -157,10 +211,11 @@ namespace BackEnd.Contollers
         }
         // ==================================================================================================== //
 
-        // JoinResults table CRUD
-
+        // JoinResults table
         // TODO: appears to be working, unsure if "id" is correct
         // URL: https://localhost:7262/api/WallStreetBets/joinresults?username=coloritoj
+	
+	// function reads list of JoinResults.
         [Route("joinresults")]
         [HttpGet]
         public IEnumerable<JoinResults> GetJoins(string username)
@@ -170,9 +225,10 @@ namespace BackEnd.Contollers
         }
         // ==================================================================================================== //
         
-        // Web API CRUD
-
+        // Web API
         // NOTE: Jeff's code here
+	
+	// nbshare custom API for reddit.com/r/WallStreetBets
         [Route("nbshare")]
         [HttpGet]
         public async Task<string> get()
@@ -191,6 +247,7 @@ namespace BackEnd.Contollers
             return json;
         }
 
+	// MarketStack API
         [Route("marketstack")]
         [HttpGet]
         public async Task<MarketStackObject> getMarketStackInfo()
