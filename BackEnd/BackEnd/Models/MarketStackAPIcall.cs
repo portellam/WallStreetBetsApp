33728b2c70231cf7ada7c24a16745c73e44ee750
portellam <amportell.13@gmail.com>
2022-02-16 02:20:49 -0500

    BackEnd: reformatted Models

--- BackEnd/BackEnd/Models/MarketStackAPIcall.cs
+++ BackEnd/BackEnd/Models/MarketStackAPIcall.cs
@@ -1,58 +1,57 @@
 ﻿namespace BackEnd.Models
 {
-    public class MarketStackAPIcall
-    {
-        // PROPERTIES //
-        private static HttpClient _realMarketStackClient = null;
+	public class MarketStackAPIcall
+	{
+		// PROPERTIES //
+		private static HttpClient _realClient = null;
 
-        // METHODS //
-        public static HttpClient MyMarketStackHttp
-        {
-            get
-            {
-                if (_realMarketStackClient == null)
-                {
-                    _realMarketStackClient = new HttpClient();
-                    _realMarketStackClient.BaseAddress = new Uri("http://api.marketstack.com/v1/eod");  // MarketStack API URL
-                }
-                return _realMarketStackClient;
-            }
-        }
+		// METHODS //
+		public static HttpClient _HttpClient
+		{
+			get
+			{
+				if (_realClient == null)
+				{
+					_realClient = new HttpClient();
+					_realClient.BaseAddress = new Uri("http://api.marketstack.com/v1/eod");  // MarketStack API URL
+				}
+				return _realClient;
+			}
+		}
 
-        // function returns StockInfo of a given Ticker
-        public static async Task<BaseInfo> GetStockInfo(string apiKey, string theTicker)
-        {
-            var connection = await MyMarketStackHttp.GetAsync($"?access_key={apiKey}&symbols={theTicker}");
-            BaseInfo myBaseInfo = await connection.Content.ReadAsAsync<BaseInfo>();
-            return myBaseInfo;
-        }
-    }
+		// function returns BaseInfo of a given Ticker
+		public static async Task<BaseInfo> GetMarketStackInfo(string apiKey, string _ticker)
+		{
+			var connection = await _HttpClient.GetAsync($"?access_key={apiKey}&symbols={_ticker}");
+			BaseInfo _BaseInfo = await connection.Content.ReadAsAsync<BaseInfo>();
+			return _BaseInfo;
+		}
+	}
 
-    public class BaseInfo
-    {
-        // PROPERTIES //
-        public Pagination pagination { get; set; }
-        public List<StockInfo> data { get; set; }
-    }
+	public class BaseInfo
+	{
+		// PROPERTIES //
+		public Pagination pagination { get; set; }
+		public List<StockInfo> data { get; set; }
+	}
 
-    public class Pagination
-    {
-        // PROPERTIES //
-        public int limit { get; set; }
-        public int offset { get; set; }
-        public int count { get; set; }
-        public int total { get; set; }
-    }
+	public class Pagination
+	{
+		// PROPERTIES //
+		public int limit { get; set; }
+		public int offset { get; set; }
+		public int count { get; set; }
+		public int total { get; set; }
+	}
 
-    
-    public class StockInfo
-    {
-        // PROPERTIES //
-        public string symbol { get; set; }
-        public double open { get; set; }
-        public double high { get; set; }
-        public double low { get; set; }
-        public double close { get; set; }
-        public double volume { get; set; }
-    }
+	public class StockInfo
+	{
+		// PROPERTIES //
+		public string symbol { get; set; }
+		public double open { get; set; }
+		public double high { get; set; }
+		public double low { get; set; }
+		public double close { get; set; }
+		public double volume { get; set; }
+	}
 }
--- BackEnd/BackEnd/Models/WSBAPIcall.cs
+++ BackEnd/BackEnd/Models/WSBAPIcall.cs
@@ -1,50 +1,52 @@
 ﻿namespace BackEnd.Models
 {
-    public class WSBAPIcall
-    {
-        // PROPERTIES //
-        private static HttpClient _realClientWSB = null;
+	public class WSBAPIcall
+	{
+		// NOTE: made changes and proposed changes to variable names to another pattern.
 
-        // METHODS //
-        public static HttpClient MyWSBHttp
-        {
-            get
-            {
-                if (_realClientWSB == null)
-                {
-                    _realClientWSB = new HttpClient();
-                    _realClientWSB.BaseAddress = new Uri("https://dashboard.nbshare.io/");  // WallStreetBets API URL
-                }
-                return _realClientWSB;
-            }
-        }
+		// PROPERTIES //
+		private static HttpClient _realClient = null;	// TODO: change?
 
-        // function returns API result of a given Ticker
-        public static async Task<WSBObject> GetWSBObject(string ticker)
-        {
-            var connection = await MyWSBHttp.GetAsync("/api/v1/apps/reddit");
-            List<WSBObject> WSBObjects = await connection.Content.ReadAsAsync<List<WSBObject>>();
+		// METHODS //
+		public static HttpClient _HttpClient
+		{
+			get
+			{
+				if (_realClient == null)
+				{
+					_realClient = new HttpClient();
+					_realClient.BaseAddress = new Uri("https://dashboard.nbshare.io/");  // WallStreetBets API URL
+				}
+				return _realClient;
+			}
+		}
 
-            WSBObject myWSBObject = new WSBObject();
+		// function returns API result of a given Ticker
+		public static async Task<WSBObject> GetWSBObject(string _ticker)
+		{
+			var connection = await _HttpClient.GetAsync("/api/v1/apps/reddit");
+			List<WSBObject> WSBObjects = await connection.Content.ReadAsAsync<List<WSBObject>>();
 
-            for (int i = 0; i < WSBObjects.Count; i++)
-            {
-                if (WSBObjects[i].ticker.ToLower() == ticker.ToLower())
-                {
-                    myWSBObject = WSBObjects[i];
-                }
-            }
-            return myWSBObject;
-        }
-    }
+			WSBObject _WSBObject = new WSBObject();
 
-  
+			for (int i = 0; i < WSBObjects.Count; i++)
+			{
+				if (_ticker.ToLower() == WSBObjects[i].ticker.ToLower())
+				{
+					_WSBObject = WSBObjects[i];
+				}
+			}
+			return _WSBObject;
+		}
+	}
+
+	public class WSBObject
+	{
+		// PROPERTIES //
+		public int no_of_comments { get; set; }
+		public string sentiment { get; set; }
+		public decimal sentiment_score { get; set; }
+		public string ticker { get; set; }
+	}
 }
-  public class WSBObject
-    {
-        // PROPERTIES //
-        public int no_of_comments { get; set; }
-        public string sentiment { get; set; }
-        public decimal sentiment_score { get; set; }
-        public string ticker { get; set; }
-    }
+
