4d29bd15d74738221031acb21625ac5bc254a9f9
Alex Portell <amportell.13@gmail.com>
2023-07-26 01:50:15 -0400

    Created unit test class for controller.

---
+++ BackEnd/BackEnd/ControllerTests/WallStreetBetsControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using BackEnd.Contollers;
+using BackEnd.Models;
+
+namespace BackEnd.ControllerTests
+{
+    [TestFixture]
+    public class WallStreetBetsControllerTests
+    {
+        private WallStreetBetsController wallStreetBetsController;
+        private WallStreetBetsDbContext wallStreetBetsDbContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            wallStreetBetsDbContext = new WallStreetBetsDbContext();
+            wallStreetBetsController = new WallStreetBetsController(wallStreetBetsDbContext);
+        }
+
+        [Test]
+        public void Test_DoFail_WillFail()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.Fail();
+        }
+    }
+}
