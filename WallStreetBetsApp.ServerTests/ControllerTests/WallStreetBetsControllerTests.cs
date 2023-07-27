eae8d4228a90c1c3323e5a79e9b9f3365fb0f9ee
Alex Portell <amportell.13@gmail.com>
2023-07-27 16:40:32 -0400

    Added test for one method.

--- WallStreetBetsApp.ServerTests/ControllerTests/WallStreetBetsControllerTests.cs
+++ WallStreetBetsApp.ServerTests/ControllerTests/WallStreetBetsControllerTests.cs
@@ -26,5 +26,39 @@ namespace WallStreetBetsApp.Server.ControllerTests
             // assert
             Assert.Pass();
         }
+
+        //IsMarketStackApiNotExpired_ApiKeyIsExpired_ReturnFalse        // NOTE: this depends if the actual API key is expired. TODO: mock?
+        //IsMarketStackApiNotExpired_ApiKeyIsEmptyOrNull_ReturnFalse
+        //IsMarketStackApiNotExpired_ApiKeyIsNotExpired_ReturnTrue
+
+        //GetMarketStackHttpClient_BaseAddressIsApiUrl_ReturnHttpClient
+
+        //UpdateLastKnownGoodMarketStackApiKey_AllApiKeysAreExpired_ThrowException
+
+        //[Test]
+        //public void UpdateLastKnownGoodMarketStackApiKey_ApiKeyListAreExpired_ThrowException()
+        //{
+        //}
+
+        [TestCase("abcdefghijklmnopqrstuvwxyz012345", "APPL")]
+        [TestCase("abcdefghijklmnopqrstuvwxyz012345", "")]
+        [TestCase("abcdefghijklmnopqrstuvwxyz012345", null)]
+        [TestCase("", "APPL")]
+        [TestCase("", "")]
+        [TestCase("", null)]
+        [TestCase(null, "APPL")]
+        [TestCase(null, "")]
+        [TestCase(null, null)]
+        public void GetMarketStackApiRequest_ReturnStringTest(string marketStackApiKey, string ticker)
+        {
+            // arrange
+            string expected = String.Format($"eod?access_key={0}&symbols={1}&limit=1", marketStackApiKey, ticker);
+
+            // act
+            var actual = wallStreetBetsController.GetMarketStackApiRequest(marketStackApiKey, ticker);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
