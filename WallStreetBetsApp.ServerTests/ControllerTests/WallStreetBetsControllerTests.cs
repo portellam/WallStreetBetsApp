594674129a672b203ec2df54f521b65f6d33fa23
Alex Portell <amportell.13@gmail.com>
2023-07-28 01:19:03 -0400

    Newest test does not work. Installed Moq package.

--- WallStreetBetsApp.ServerTests/ControllerTests/WallStreetBetsControllerTests.cs
+++ WallStreetBetsApp.ServerTests/ControllerTests/WallStreetBetsControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using WallStreetBetsApp.Server.Controllers;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using WallStreetBetsApp.Server.Controllers;
 using WallStreetBetsApp.Server.Models;
 
 namespace WallStreetBetsApp.Server.ControllerTests
@@ -8,23 +10,42 @@ namespace WallStreetBetsApp.Server.ControllerTests
     {
         private WallStreetBetsController wallStreetBetsController;
         private WallStreetBetsDbContext wallStreetBetsDbContext;
+        private Mock<WallStreetBetsController> mockWallStreetBetsController;
+        private Mock<WallStreetBetsDbContext> mockWallStreetBetsDbContext;
+        private Mock<DbSet<NoteModel>> mockNoteDbSet;
+
+        private static List<NoteModel> noteList = new List<NoteModel>
+        {
+            new NoteModel()
+            {
+                Id = 0,
+                Description = "First note"
+            },
+            new NoteModel()
+            {
+                Id = 1,
+                Description = "Second note"
+            },
+            new NoteModel()
+            {
+                Id = 2,
+                Description = "Third note"
+            }
+        };
 
         [SetUp]
         public void SetUp()
         {
-            wallStreetBetsDbContext = new WallStreetBetsDbContext();
-            wallStreetBetsController = new WallStreetBetsController(wallStreetBetsDbContext);
+            wallStreetBetsController = new WallStreetBetsController(new WallStreetBetsDbContext());
+            mockWallStreetBetsController = new Mock<WallStreetBetsController>(new WallStreetBetsDbContext());
         }
 
-        [Test]
-        public void Test_DoFail_WillPass()
+        private static IEnumerable<TestCaseData> NoteListTestCaseData()
         {
-            // arrange
-
-            // act
-
-            // assert
-            Assert.Pass();
+            foreach (NoteModel noteModel in noteList)
+            {
+                yield return new TestCaseData(noteModel);
+            };
         }
 
         //IsMarketStackApiNotExpired_ApiKeyIsExpired_ReturnFalse        // NOTE: this depends if the actual API key is expired. TODO: mock?
@@ -35,10 +56,25 @@ namespace WallStreetBetsApp.Server.ControllerTests
 
         //UpdateLastKnownGoodMarketStackApiKey_AllApiKeysAreExpired_ThrowException
 
-        //[Test]
-        //public void UpdateLastKnownGoodMarketStackApiKey_ApiKeyListAreExpired_ThrowException()
-        //{
-        //}
+        [TestCaseSource(nameof(NoteListTestCaseData))]
+        public void DeleteNote_MatchNoteId_DoDeleteNoteAndSaveChangesTest(NoteModel noteModel)              // NOTE: this test fails.
+        {
+            // arrange
+            noteList.ForEach(noteModel => mockWallStreetBetsDbContext.Object.NoteDbSet.Add(noteModel));     // NOTE: this throws a null ref exception.
+            mockWallStreetBetsDbContext.Setup(x => x.NoteDbSet.ToList()).Returns(noteList).Verifiable();
+            mockWallStreetBetsDbContext.Setup(x => x.NoteDbSet.Remove(noteModel)).Verifiable();
+            mockWallStreetBetsDbContext.Setup(x => x.SaveChanges()).Verifiable();
+
+            // act
+            wallStreetBetsController.DeleteNote(noteModel.Id);
+            var result = mockWallStreetBetsDbContext.Object.NoteDbSet.Contains(noteModel);
+
+            // assert
+            mockWallStreetBetsDbContext.Verify(x => x.Remove(noteModel), Times.Once);
+            mockWallStreetBetsDbContext.Verify(x => x.SaveChanges(), Times.Once);
+            Assert.AreNotEqual(mockWallStreetBetsDbContext.Object.NoteDbSet, noteList);
+            Assert.IsFalse(result);            
+        }
 
         [TestCase("abcdefghijklmnopqrstuvwxyz012345", "APPL")]
         [TestCase("abcdefghijklmnopqrstuvwxyz012345", "")]
